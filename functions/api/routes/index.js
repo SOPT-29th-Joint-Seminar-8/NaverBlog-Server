0f6683d3ce8f5541185162cf6c03b21d3896cf66
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-11-24 17:00:13 +0900

    [ADD] initial setting

---
+++ functions/api/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// router.use('/post', require('./post'));
+// router.use('/comment', require('./comment'));
+
+module.exports = router;
---
+++ functions/db/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // postDB: require('./post'),
+    // commentDB: require('./comment'),
+  };
