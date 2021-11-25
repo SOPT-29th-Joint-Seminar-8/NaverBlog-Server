7f47ae5984c0b74973ca82c95ffc4b7b839d6be0
yaeoni <tjghwnd103@gmail.com>
2021-11-26 04:26:51 +0900

    [FEAT] 게시글 하트 날리기/ 취소하기 API routing

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/main', require('./mainGET'));
-
+router.patch('/:post_id/like', require('./likePATCH'));
 module.exports = router;
